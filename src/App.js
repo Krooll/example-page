3ef0ac44afcaa00d0fea88ed49180534d7810dde
Pawel Krol <paullkroll@icloud.com>
2023-11-06 20:17:29 +0100

    End work api data products, everything works - store and data done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import { useDispatch, useSelector } from "react-redux";
-import { fetchProducts, getAllProducts } from "./redux/productsRedux";
-import { useEffect } from "react";
 
 const App = () => {
-  const dispatch = useDispatch();
-  const products = useSelector(getAllProducts);
-
-  useEffect(() => {
-    dispatch(fetchProducts());
-}, [dispatch]);
 
   return (
     <div>
-      {products.map(product => <li key={product.id}>{product.title} <img src={product.icon}/></li>)}
+      Hello world
     </div>
   );
 }
