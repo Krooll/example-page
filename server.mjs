4509abf866a095f17bbcda76eb3ce8753bdcbddd
Pawel Krol <paullkroll@icloud.com>
2023-11-18 21:59:25 +0100

    Add build catalog

--- server.mjs
+++ server.mjs
@@ -1,17 +1,23 @@
 import jsonServer from 'json-server';
+import path from 'path';
+
 const server = jsonServer.create();
-const router = jsonServer.router('build/db/app.json');
+const dbPath = path.join(__dirname, 'build/db/app.json'); // Łączenie ścieżki do pliku app.json
+const router = jsonServer.router(dbPath);
 const middlewares = jsonServer.defaults({
   static: 'build',
   noCors: true
 });
 const port = process.env.PORT || 3131;
+
 server.use(middlewares);
 server.use(jsonServer.rewriter({
   '/api/*': '/$1'
 }));
 
 server.use(router);
-server.listen(port);
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
 
 /*start on replit: node server.mjs*/
