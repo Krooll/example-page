6dfa6f76f213613311f7c09f36f466aaca125b9d
Pawel Krol <paullkroll@icloud.com>
2023-11-18 22:43:54 +0100

    Rebuild server.mjs, delete build from gitgnore

--- server.mjs
+++ server.mjs
@@ -1,23 +1,17 @@
 import jsonServer from 'json-server';
-import cors from 'cors';
-import path from 'path';
-
 const server = jsonServer.create();
-const dbPath = path.join(__dirname, 'build/db/app.json'); // Łączenie ścieżki do pliku app.json
-const router = jsonServer.router(dbPath);
-
-// Dodaj obsługę CORS
-server.use(cors());
-
+const router = jsonServer.router('build/db/app.json');
+const middlewares = jsonServer.defaults({
+  static: 'build',
+  noCors: true
+});
 const port = process.env.PORT || 3131;
-
+server.use(middlewares);
 server.use(jsonServer.rewriter({
   '/api/*': '/$1'
 }));
 
 server.use(router);
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
-});
+server.listen(port);
 
 /*start on replit: node server.mjs*/
