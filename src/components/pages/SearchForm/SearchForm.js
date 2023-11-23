d7d2ec62bc8e7e0bd326df1e477feefb23d2addf
Pawel Krol <paullkroll@icloud.com>
2023-11-23 22:51:30 +0100

    End work for searchfrom, function filter product done, as card use productcard

--- src/components/pages/SearchForm/SearchForm.js
+++ src/components/pages/SearchForm/SearchForm.js
@@ -7,12 +7,28 @@ import { useDispatch } from 'react-redux';
 import { useEffect, useState } from 'react';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import ProductCard from '../../common/ProductCard/ProductCard';
 
 const SearchForm = () => {
     /*FontAwesome*/
     const searchIcon = <FontAwesomeIcon className={styles.link}icon={faMagnifyingGlass} />
     const productList = useSelector(getAllProducts);
     const [search, setSearch] = useState('');
+    const [result, setResult] = useState([]);
+    
+
+    const filterProducts = (e) => {
+        const filter = productList.filter(product => product.title.toLowerCase().includes(search.toLowerCase()));
+        e.preventDefault();
+        if(filter.length){
+            setResult(filter);
+            console.log('result', result);
+            setSearch('');
+        }else{
+            setResult([]);
+            setSearch('');
+        }        
+    };
 
     const dispatch = useDispatch();
     useEffect(() => {
@@ -33,9 +49,12 @@ const SearchForm = () => {
                     <Col xs={6} md={5} lg={3} >
                         <FormControl onChange={e => setSearch(e.target.value)} value={search} placeholder="Szukaj..." />
                     </Col> 
-                    <button className={styles.button}>{searchIcon}</button>
+                    <button onClick={filterProducts} className={styles.button}>{searchIcon}</button>
                 </Col>
             </Form>
+            <Col xs={12} md={12} lg={12} className={styles.result}>
+                {result.map(item => <ProductCard key={item.id} {...item} />)}
+            </Col>
         </Container>
     );
 };
